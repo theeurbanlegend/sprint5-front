cccc3e0a6f110c57f33b492332cfcc32caa2eade
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-08-25 11:02:35 +0300

    Update apiSlice.jsx

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://back-6xof.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
