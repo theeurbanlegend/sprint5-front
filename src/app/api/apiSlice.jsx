c5f9a12cb77b37b138b9ca619c24411c684e0e6e
Bewton Maina <134442464+theeurbanlegend@users.noreply.github.com>
2023-10-27 19:47:53 +0300

    Update apiSlice.jsx

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://back-6xof.onrender.com',
+    baseUrl: 'https://sprint5-backend.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
